53738cdb4d0a56ead08668f30c70caea7d368e66
oburu <oburusolo@gmail.com>
2017-04-20 10:16:43 +0100

    change to stateless, component total reporting

--- bundle.js
+++ bundle.js
@@ -20347,97 +20347,73 @@
 	  value: true
 	});
 
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 	var _react = __webpack_require__(2);
 
 	var _react2 = _interopRequireDefault(_react);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	var TotalReporting = function TotalReporting(props) {
+	  var totalSales = { sales: 0 };
+	  var price = 1.65;
 
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var TotalReporting = function (_Component) {
-	  _inherits(TotalReporting, _Component);
-
-	  function TotalReporting() {
-	    _classCallCheck(this, TotalReporting);
-
-	    return _possibleConstructorReturn(this, (TotalReporting.__proto__ || Object.getPrototypeOf(TotalReporting)).apply(this, arguments));
+	  if (props.sales.length > 0) {
+	    totalSales = props.sales.reduce(function (a, b) {
+	      return { sales: a.sales + b.sales };
+	    });
 	  }
-
-	  _createClass(TotalReporting, [{
-	    key: "render",
-	    value: function render() {
-	      var totalSales = { sales: 0 };
-	      var price = 1.65;
-
-	      if (this.props.sales.length > 0) {
-	        totalSales = this.props.sales.reduce(function (a, b) {
-	          return { sales: a.sales + b.sales };
-	        });
-	      }
-
-	      return _react2.default.createElement(
+	  return _react2.default.createElement(
+	    "div",
+	    { className: "col col-left" },
+	    _react2.default.createElement(
+	      "h4",
+	      null,
+	      "Total reporting"
+	    ),
+	    _react2.default.createElement(
+	      "p",
+	      null,
+	      "Report of gum sold in the state of California, USA. All this data is coming from a sleepy RESTful api. "
+	    ),
+	    _react2.default.createElement(
+	      "div",
+	      { className: "row" },
+	      _react2.default.createElement(
 	        "div",
-	        { className: "col col-left" },
+	        { className: "col-sm-6" },
 	        _react2.default.createElement(
-	          "h4",
+	          "h5",
 	          null,
-	          "Total reporting"
+	          "Money Income (Price: \xA3 ",
+	          price,
+	          " each)"
 	        ),
 	        _react2.default.createElement(
-	          "p",
+	          "h1",
 	          null,
-	          "Report of gum sold in the state of California, USA. All this data is coming from a sleepy RESTful api. "
-	        ),
-	        _react2.default.createElement(
-	          "div",
-	          { className: "row" },
-	          _react2.default.createElement(
-	            "div",
-	            { className: "col-sm-6" },
-	            _react2.default.createElement(
-	              "h5",
-	              null,
-	              "Money Income (Price: \xA3 ",
-	              price,
-	              " each)"
-	            ),
-	            _react2.default.createElement(
-	              "h1",
-	              null,
-	              "\xA3 ",
-	              (totalSales.sales * price).toFixed(2)
-	            )
-	          ),
-	          _react2.default.createElement(
-	            "div",
-	            { className: "col-sm-6" },
-	            _react2.default.createElement(
-	              "h5",
-	              null,
-	              "Amount Sold"
-	            ),
-	            _react2.default.createElement(
-	              "h1",
-	              null,
-	              " ",
-	              totalSales.sales,
-	              " "
-	            )
-	          )
+	          "\xA3 ",
+	          (totalSales.sales * price).toFixed(2)
 	        )
-	      );
-	    }
-	  }]);
-
-	  return TotalReporting;
-	}(_react.Component);
+	      ),
+	      _react2.default.createElement(
+	        "div",
+	        { className: "col-sm-6" },
+	        _react2.default.createElement(
+	          "h5",
+	          null,
+	          "Amount Sold"
+	        ),
+	        _react2.default.createElement(
+	          "h1",
+	          null,
+	          " ",
+	          totalSales.sales,
+	          " "
+	        )
+	      )
+	    )
+	  );
+	};
 
 	exports.default = TotalReporting;
 
--- src/components/total_reporting.js
+++ src/components/total_reporting.js
@@ -1,31 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class TotalReporting extends Component {
-  render(){
-    let totalSales = {sales:0};
-    let price = 1.65;
+const TotalReporting = (props) =>{
+  let totalSales = {sales:0};
+  let price = 1.65;
 
-    if(this.props.sales.length > 0){
-      totalSales = this.props.sales.reduce((a, b) => {
-        return {sales: a.sales + b.sales}
-      });
-    }
-
-    return (
-      <div className="col col-left">
-        <h4>Total reporting</h4>
-        <p>Report of gum sold in the state of California, USA. All this data is coming from a sleepy RESTful api. </p>
-        <div className="row">
-          <div className="col-sm-6">
-            <h5>Money Income (Price: £ {price} each)</h5>
-            <h1>£ {(totalSales.sales * price).toFixed(2)}</h1>
-          </div>
-          <div className="col-sm-6">
-            <h5>Amount Sold</h5>
-            <h1> {totalSales.sales} </h1>
-          </div>
+  if(props.sales.length > 0){
+    totalSales = props.sales.reduce((a, b) => {
+      return {sales: a.sales + b.sales}
+    });
+  }
+  return (
+    <div className="col col-left">
+      <h4>Total reporting</h4>
+      <p>Report of gum sold in the state of California, USA. All this data is coming from a sleepy RESTful api. </p>
+      <div className="row">
+        <div className="col-sm-6">
+          <h5>Money Income (Price: £ {price} each)</h5>
+          <h1>£ {(totalSales.sales * price).toFixed(2)}</h1>
+        </div>
+        <div className="col-sm-6">
+          <h5>Amount Sold</h5>
+          <h1> {totalSales.sales} </h1>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
 }
+
+export default TotalReporting;
